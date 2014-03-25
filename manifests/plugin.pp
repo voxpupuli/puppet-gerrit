766a48aca6e0845645dcfd3ff9ae3e5a271314e9
Yanis Guenane <yanis.guenane@enovance.com>
2014-03-25 17:14:26 -0400

    Add the possiblity to add plugins the same way we add hooks.
    This commit aims to allow a user to add plugins the same way one add
    hooks.
    
    Please see #7 as it depends on it

---
+++ manifests/plugin.pp
@@ -0,0 +1,16 @@
+define gerrit::plugin(
+  $source,
+  $ensure = 'present',
+){
+
+  file{
+    "${gerrit::target}/plugins/${name}":
+      ensure   => $ensure,
+      source   => $source,
+      owner    => $gerrit::user,
+      group    => $gerrit::user,
+      mode     => '0700',
+      require  => [Exec['install_gerrit'], Gerrit::Folder['plugins']],
+  }
+
+}
