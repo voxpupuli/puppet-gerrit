a4a676f3f25ea97626fd0e910962be07ca0f0f46
Tim Meusel <tim@bastelfreak.de>
2016-11-03 00:15:01 +0100

    rubocop: fix RSpec/ImplicitExpect

--- spec/classes/gerrit_spec.rb
+++ spec/classes/gerrit_spec.rb
@@ -14,43 +14,43 @@ describe 'gerrit', type: :class do
             target: '/srv/gerrit'
           }
         end
-        it { should compile.with_all_deps }
-        it { should contain_class('gerrit') }
-        it { should contain_class('gerrit::params') }
-        it { should contain_user('gerrit') }
-        it { should contain_service('gerrit') }
-        it { should contain_exec('config_auth.type') }
-        it { should contain_exec('config_database.database') }
-        it { should contain_exec('config_database.type') }
-        it { should contain_exec('config_download.scheme') }
-        it { should contain_exec('config_download.scheme_empty') }
-        it { should contain_exec('config_gerrit.canonicalWebUrl') }
-        it { should contain_exec('reload_gerrit') }
-        it { should contain_exec('config_gitweb.cgi') }
-        it { should contain_exec('config_httpd.listenUrl') }
-        it { should contain_exec('install_gerrit') }
-        it { should contain_file('/srv/gerrit/hooks') }
-        it { should contain_file('/srv/gerrit/lib/mysql-connector-java.jar') }
-        it { should contain_file('/srv/gerrit/plugins') }
-        it { should contain_package('git') }
-        it { should contain_package('gitweb') }
-        it { should contain_gerrit__config('auth.type') }
-        it { should contain_gerrit__config('database.database') }
-        it { should contain_gerrit__config('database.type') }
-        it { should contain_gerrit__config('download.scheme') }
-        it { should contain_gerrit__config('gerrit.canonicalWebUrl') }
-        it { should contain_gerrit__config('gitweb.cgi') }
-        it { should contain_gerrit__config('httpd.listenUrl') }
-        it { should contain_gerrit__folder('hooks') }
-        it { should contain_gerrit__folder('plugins') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('gerrit') }
+        it { is_expected.to contain_class('gerrit::params') }
+        it { is_expected.to contain_user('gerrit') }
+        it { is_expected.to contain_service('gerrit') }
+        it { is_expected.to contain_exec('config_auth.type') }
+        it { is_expected.to contain_exec('config_database.database') }
+        it { is_expected.to contain_exec('config_database.type') }
+        it { is_expected.to contain_exec('config_download.scheme') }
+        it { is_expected.to contain_exec('config_download.scheme_empty') }
+        it { is_expected.to contain_exec('config_gerrit.canonicalWebUrl') }
+        it { is_expected.to contain_exec('reload_gerrit') }
+        it { is_expected.to contain_exec('config_gitweb.cgi') }
+        it { is_expected.to contain_exec('config_httpd.listenUrl') }
+        it { is_expected.to contain_exec('install_gerrit') }
+        it { is_expected.to contain_file('/srv/gerrit/hooks') }
+        it { is_expected.to contain_file('/srv/gerrit/lib/mysql-connector-java.jar') }
+        it { is_expected.to contain_file('/srv/gerrit/plugins') }
+        it { is_expected.to contain_package('git') }
+        it { is_expected.to contain_package('gitweb') }
+        it { is_expected.to contain_gerrit__config('auth.type') }
+        it { is_expected.to contain_gerrit__config('database.database') }
+        it { is_expected.to contain_gerrit__config('database.type') }
+        it { is_expected.to contain_gerrit__config('download.scheme') }
+        it { is_expected.to contain_gerrit__config('gerrit.canonicalWebUrl') }
+        it { is_expected.to contain_gerrit__config('gitweb.cgi') }
+        it { is_expected.to contain_gerrit__config('httpd.listenUrl') }
+        it { is_expected.to contain_gerrit__folder('hooks') }
+        it { is_expected.to contain_gerrit__folder('plugins') }
 
         case facts[:operatingsystem]
         when 'Debian', 'Ubuntu'
-          it { should contain_package('libmysql-java') }
-          it { should contain_package('default-jdk') }
+          it { is_expected.to contain_package('libmysql-java') }
+          it { is_expected.to contain_package('default-jdk') }
         when 'Redhat', 'RedHat'
-          it { should contain_package('mysql-connector-java') }
-          it { should contain_package('java-1.6.0-openjdk') }
+          it { is_expected.to contain_package('mysql-connector-java') }
+          it { is_expected.to contain_package('java-1.6.0-openjdk') }
         end
       end
     end
