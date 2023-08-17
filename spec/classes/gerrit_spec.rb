020ee7b91c778ded02c52b288f09c6262ef9d5bc
Tim Meusel <tim@bastelfreak.de>
2023-08-18 00:15:58 +0200

    rubocop: autofix

--- spec/classes/gerrit_spec.rb
+++ spec/classes/gerrit_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'gerrit', type: :class do
   on_supported_os.each do |os, facts|
-    context "on #{os} " do
+    context "on #{os}" do
       let :facts do
         facts
       end
